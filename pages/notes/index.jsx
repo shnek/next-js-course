f6e7f41ff4892cee8c5a9b86748b32169e56cc65
Shnek <shnek94@gmail.com>
2021-11-05 09:30:18 -0400

    Adding links

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Page = () => <div>Note index path</div>
+const Page = () => (
+    <div>
+        <h1>Notes page</h1>
+        <Link href="/notes/[id]" as={`/notes/1`}>
+            <a></a>
+        </Link>
+    </div>
+)
 
 export default Page
