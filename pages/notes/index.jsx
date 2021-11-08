3a00e1164f96e96204b4398c899f5e17467d94b7
Shnek <shnek94@gmail.com>
2021-11-08 06:06:58 -0500

    Fixing themes/CSS

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import Link from 'next/link'
 
-const Page = () => {
-    const notes = new Array(15).fill().map((e,i) => ({id: i, title: `Note: ${i}`}))
+export default () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `This is my note ${i}`}))
 
-    return (
-    <div>
-        <h1>Notes page</h1>
+  return (
+    <div sx={{variant: 'containers.page'}}>
+      <h1>My Notes</h1>
+
+      <div sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap'}}>
         {notes.map(note => (
-            <div>
-                <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
-                    <a>
-                        {note.title}
-                    </a>
-                </Link>
-            </div>
+          <div sx={{width: '33%', p: 2}}>
+            <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+              <a sx={{textDecoration: 'none', cursor: 'pointer'}}>
+                <div sx={{variant: 'containers.card',}}>
+                  <strong>{note.title}</strong>
+                </div>
+              </a>
+            </Link>
+          </div>
         ))}
+      </div>
     </div>
-    )
+  )
 }
-
-export default Page
