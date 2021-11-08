a522954fbb737ec5fb926b4ac2e5fb023e6d16cf
Shnek <shnek94@gmail.com>
2021-11-08 06:49:49 -0500

    Adding environment variables to the config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const {PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER} = require('next/constants')
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
 
-module.exports = (phase, {defaultConfig}) => {
-    if(phase === PHASE_DEVELOPMENT_SERVER){
-        console.log("I'm in dev mode here");
+dotenvLoad()
 
-
-        return defaultConfig
-    }
-
-    return defaultConfig
-}
+const withEnv = nextEnv();
+module.exports = withEnv()
