0651d39ac8690c4c4ce9a2e2b37c7005da43d568
Shnek <shnek94@gmail.com>
2021-11-08 05:15:28 -0500

    Adding multiple notes

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
 
-const Page = () => (
+const Page = () => {
+    const notes = new Array(15).fill().map((e,i) => ({id: i, title: `Note: ${i}`}))
+
+    return (
     <div>
         <h1>Notes page</h1>
-        <Link href="/notes/[id]" as={`/notes/1`}>
-            <a></a>
-        </Link>
+        {notes.map(note => (
+            <div>
+                <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+                    <a>
+                        {note.title}
+                    </a>
+                </Link>
+            </div>
+        ))}
     </div>
-)
+    )
+}
 
 export default Page
