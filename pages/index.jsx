6a61b947f5a888f96ecb100d11682bac2c3f6bc9
Shnek <shnek94@gmail.com>
2021-11-09 04:42:48 -0500

    Adding dynamic/client side component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,17 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import Link from 'next/link'
+//import Browser from '../src/components/browser'
+import dynamic from 'next/dynamic'
+
+const BrowserComponent = dynamic(() => import('../src/components/browser'), {ssr: false})
 
 export default ({content}) => (
   <div sx={{ height: `calc(100vh - 60px)` }}>
     <div sx={{ variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%' }}>
       <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
     </div>
+    <BrowserComponent/>
   </div>
 )
 
